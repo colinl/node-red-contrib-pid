680200045d17a76981ba83c8137262092c0cbe85
colinl <colin@clanlaw.org.uk>
2016-11-14 14:36:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+node-red-contrib-pid
+=====================
+
+A [Node-RED] node which operates as a PID loop controller node intended for the control of real world processes..
+
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-contrib-pid
+
+
+Usage
+-----
+
+Pass the node a process value in **msg.payload** at regular intervals and configure (or pass in via a message) a setpoint and tuning parameters and the algorithm will generate a required power output between 0 and 1 in **msg.payload** using a Proportional+Integral+Derivative algorithm.
+    
+Any message received with a **msg.topic** other than those defined below is assumed to contain the current process value in **msg.payload** and the control algorithm will be run, passing on the required power in **msg.payload**.
+
+
+Configuration
+-------------
+
+  * **Setpoint** - This is the process value that the process is aiming for. It may be setup either in the configuration of the node or by passing the node a message with **msg.topic** set to `setpoint` and **msg.payload** set to the required floating point value.
+
+  * **Proportional band** - This controls the gain of the loop and is the range of process value over which the power output will go from 0 to full power. The units are that of the process and setpoint, so for example in a heating application it might be set to 1.5 degrees. It may be setup either in the configuration of the node or by passing the node a message with **msg.topic** set to `prop_band` and **msg.payload** set to the required floating point value.
+
+  * **Integral time** - This is a setting for the integral time, in seconds. It represents the time constant of the integration effect. The larger the value the slower the integral effect will be. Obviously the slower the process is the larger this should be. For example for a domestic room heated by convection radiators a setting of one or two hours might be appropriate (in seconds). To disable the integral effect set this to a large number. It may be setup either in the configuration of the node or by passing the node a message with **msg.topic** set to `t_integral` and **msg.payload** set to the required integer value.
+
+  * **Derivative time** - This is a setting for the derivative time, in seconds. It represents the time constant of the derivative effect. The larger the value the greater will be the derivative effect. Typically this will be set to somewhat less than 25% of the integral setting, once the integral has been adjusted to the optimum value. To disable the derivative effect set this to 0. When initially tuning a loop it is often sensible to start with derivative zero and wind it in once other parameters have been setup. It may be setup either in the configuration of the node or by passing the node a message with **msg.topic** set to `t_derivative` and **msg.payload** set to the required integer value.
+
+  * **Initial integral** - This is an initial value which is used to preset the integrated error value when the flow is deployed in order to assist in homing in on the setpoint the first time. It should be set to an estimate of what the power requirement might be in order to maintain the process at the setpoint. For example for a domestic room heating application it might be set to 0.2 indicating that 20% of the available power might be required to maintain the setpoint.  The value is of no consequence apart from on deployment of the flow.
+
+  * **Max sample interval** - This is the maximum time in seconds that is expected between samples. It is provided to cope with unusual situations such as a faulty sensor that might prevent the node from being supplied with a process value and to prevent the problem of integral wind-up during that time. Each time a process value is received a check is made on the time since the last sample and if too long has elapsed the integral error term is locked at the previous value and the derivative is set to zero. It may be setup either in the configuration of the node or by passing the node a message with **msg.topic** set to `max_interval` and **msg.payload** set to the required integer value.
+
+  * **Derivative smoothing factor** - In situations where the process sensor has limited resolution (such as the DS18B20), the use of deriviative can be problematic as when the process is changing only slowly the steps in the value cause spikes in the derivative. To reduce the effect of these this parameter can be set to apply a filter to the derivative term.  I have found that with the DS18B20 that a value of 3 here can be beneficial, providing effectively a low pass filter on the derivative at 1/3 of the derivative time. This feature may also be useful if the process value is particularly noisy. The smaller the value the greater the filtering effect but the more it will reduce the effectiveness of the derivative. A value of zero disables this feature. It may be setup either in the configuration of the node or by passing the node a message with **msg.topic** set to `smooth_factor` and **msg.payload** set to the required value.
+
+  * **Enable state** - This is can be used to enable or disable the control (1=enable, 0=disable). When disabled the output is set the value defined in the parameter below. Enable/disable may be setup either in the configuration of the node or by passing the node a message with **msg.topic** set to `enable` and **msg.payload** set to 1/0 or true/false.
+
+  * **Output power when disabled** - This is the value the output is set to when the loop is disabled. It may be setup either in the configuration of the node or by passing the node a message with **msg.topic** set to `disabled_op` and **msg.payload** set to the required floating point value between 0 and 1.
+
+  
+Status
+------
+
+The status text will normally be empty. In unusual conditions it will show the following values.
+
+  * **Disabled** - Control is currently disabled.
+  
+  * **Integral Locked** - The process is far from the setpoint and the integral term has been locked to prevent integral windup.
+  
+  * **Bad PV** - The process value passed in is not a number.
+  
+  * **Too long since last sample** - A long time has elapsed since the last good sample so the integral term will not be accumulated for this sample.
+
+
+[Node-RED]:  http://nodered.org/
+
---
+++ pid.js
@@ -0,0 +1,151 @@
+/**
+ * Copyright 2016 Colin Law
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+  "use strict";
+  
+  function PID(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
+    node.setpoint = Number(config.setpoint);
+    node.enable = Number(config.enable);
+    node.prop_band = Number(config.pb);
+    node.t_integral = Number(config.ti);
+    node.t_derivative = Number(config.td);
+    node.integral_default = Number(config.integral_default);
+    node.smooth_factor = Number(config.smooth_factor);
+    node.max_interval = Number(config.max_interval);
+    node.disabled_op = Number(config.disabled_op);
+   
+    this.on('input', function(msg) {
+      var newMsg = null;
+      if (msg.topic == 'setpoint') {
+        node.setpoint = Number(msg.payload);
+      } else if (msg.topic == 'enable') {
+        node.enable = Number(msg.payload);
+      } else if (msg.topic == 'prop_band') {
+        node.prop_band = Number(msg.payload);
+      } else if (msg.topic == 't_integral') {
+        node.t_integral = Number(msg.payload);
+      } else if (msg.topic == 't_derivative') {
+        node.t_derivative = Number(msg.payload);
+      } else if (msg.topic == 'smooth_factor') {
+        node.smooth_factor = Number(msg.payload);
+      } else if (msg.topic == 'max_interval') {
+        node.max_interval = Number(msg.payload);
+      } else if (msg.topic == 'disabled_op') {
+        node.disabled_op = Number(msg.payload);
+      } else {
+        // anything else is assumed to be a process value
+        node.pv = Number(msg.payload);   // this may give NaN which is handled in runControlLoop
+        newMsg = runControlLoop();
+      }
+      node.send(newMsg);
+    });
+
+    function runControlLoop() {
+      //node.log("pv, setpoint, prop_band, t_integral, t_derivative, integral_default, smooth_factor, max_interval, enable, disabled_op");
+      //node.log(node.pv + " " + node.setpoint + " " + node.prop_band + " " + node.t_integral + " " + node.t_derivative + " " + node.integral_default + " " + node.smooth_factor + " " + node.max_interval + " " + node.enable + " " + node.disabled_op);
+      var ans;
+      node.status({});
+      // check we have a good pv value
+      if (!isNaN(node.pv) && isFinite(node.pv)) {
+        // even if we are disabled (enable == 0 or false) then run through the calcs to keep the derivative up to date
+        // but lock the integral and set power to appropriate value at the end
+        var time = Date.now();
+        var integral_locked = false;
+        var factor;
+        if (node.last_sample_time) {
+          var delta_t = (time - node.last_sample_time)/1000;  // seconds
+          if (delta_t <= 0 || delta_t > node.max_interval) {
+            // too long since last sample so leave integral as is and set deriv to zero
+            node.status({fill:"red",shape:"dot",text:"Too long since last sample"});
+            node.derivative = 0
+          } else {
+            if (node.smooth_factor > 0) {
+              // A derivative smoothing factor has been supplied
+              // smoothing time constant is td/factor but with a min of delta_t to stop overflows
+              ts = Math.max(node.t_derivative/node.smooth_factor, delta_t);
+              factor = 1.0/(ts/delta_t);
+            } else {
+              // no integral smoothing so factor is 1, this makes smoothed_value the previous pv
+              factor = 1.0;
+            }
+            var delta_v = (node.pv - node.smoothed_value) * factor;
+            node.smoothed_value = node.smoothed_value + delta_v
+            //node.log( "factor " + factor.toFixed(3) + " delta_t " + delta_t + " delta_v " + delta_v.toFixed(3) +
+            //  " smoothed " + node.smoothed_value.toFixed(3));
+            node.derivative = node.t_derivative * delta_v/delta_t;
+            
+            // lock the integral if abs(previous integral + error) > prop_band/2
+            // as this means that P + I is outside the linear region so power will be 0 or full
+            // also lock if control is disabled
+            var error = node.pv - node.setpoint;
+            var pbo2 = node.prop_band/2.0;
+            if ((Math.abs(error + node.integral) < pbo2)  && node.enable) {
+              integral_locked = false;
+              node.integral = node.integral + error * delta_t/node.t_integral;
+              // clamp to +- 0.5 prop band widths so that it cannot push the zero power point outside the pb
+              if ( node.integral < -pbo2 ) {
+                node.integral = -pbo2;
+              } else if (node.integral > pbo2) {
+                node.integral = pbo2;
+              }
+            } else {
+              //node.log("Locking integral");
+              integral_locked = true;
+            }
+          }
+            
+        } else {
+            // first time through so initialise context data
+            node.smoothed_value = node.pv;
+            node.integral = node.integral_default;
+            node.derivative = 0.0;
+        }
+        
+        var proportional = node.pv - node.setpoint;
+        var power = -1.0/node.prop_band * (proportional + node.integral + node.derivative) + 0.5
+        if (power < 0.0) {
+          power = 0.0;
+        } else if (power > 1.0) {
+          power = 1.0;
+        }
+        // set power to disabled value if the loop is not enabled
+        if (!node.enable) {
+          power = node.disabled_op;
+          node.status({fill:"yellow",shape:"dot",text:"Disabled"});
+        } else if (integral_locked) {
+          node.status({fill:"green",shape:"dot",text:"Integral Locked"});
+        } else {
+          node.status({fill:"green",shape:"dot"});
+        }
+        node.last_sample_time = time;
+      } else {
+        // pv is not a good number so set power to disabled value
+        power = node.disabled_op;
+        node.status({fill:"red",shape:"dot",text:"Bad PV"});
+      }
+      ans =  {payload: power, pv: node.pv, setpoint: node.setpoint, proportional: proportional, integral: node.integral, 
+        derivative: node.derivative, smoothed_value: node.smoothed_value}
+      return ans;
+    }
+  }
+  RED.nodes.registerType("PID",PID);
+}
+
+  
+
